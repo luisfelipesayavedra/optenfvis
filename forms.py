390ca96ebe1d21b190cb3326790dc6d5777b725c
luisfelipesayavedra <luisfelipesayavedra@gmail.com>
2019-11-23 17:52:00 -0500

    adding cat logic and changing DB structure with new tabs

--- forms.py
+++ forms.py
@@ -1,11 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DecimalField, IntegerField, \
-    TextAreaField, SelectField, PasswordField, HiddenField
+from wtforms import StringField, SubmitField, DecimalField, IntegerField,TextAreaField, SelectField, PasswordField, HiddenField
 from wtforms.fields.html5 import EmailField
 from flask_wtf.file import FileField, FileRequired
 from wtforms.validators import Required, NumberRange
 
 
-class UploadForm(FlaskForm):
-    photo = FileField('selecciona imagen:', validators=[FileRequired()])
-    submit = SubmitField('Submit')
+class formCategoria(FlaskForm):
+   nombre = StringField("Nombre:",
+                        validators=[Required("Tienes que introducir el dato")]
+                        )
+   submit = SubmitField('Enviar')
+
+
+class formArticulo(FlaskForm):
+   nombre=StringField("Nombre:", validators=[Required("Tienes que introducir el dato")])
+   precio=DecimalField("Precio:", default=0, validators=[Required("Tienes que introducir el dato")])
+   descripcion= TextAreaField("Descripción:")
+   photo = FileField('Selecciona imagen:')
+   CategoriaId=SelectField("Categoría:", coerce=int)
+   submit = SubmitField('Enviar')
--- run.py
+++ run.py
@@ -1,10 +1,15 @@
-#run proporciona una cpacidad de enrutamiento de diferentes URLs para la comunicacion constante del MVC es el archivo de inicializacion y el que recibe los datos para enviarlos al(los) modelos y que este los envie directamente a la base de datos
+'''run proporciona una cpacidad de enrutamiento de diferentes
+URLs para la comunicacion constante del MVC es el archivo de
+inicializacion y el que recibe los datos para enviarlos al(los)
+ modelos y que este los envie directamente a la base de datos'''
 import os
 from flask import Flask, render_template,request, redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from flask_wtf.csrf import CSRFProtect, CSRFError
 import config
+from forms import formArticulo, formCategoria
+
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -26,34 +31,72 @@ def articulos():
     return 'lista de articulos'
 
 
-@app.route('/articulos/new', methods=["POST"])
-def articulos_new():
-    return 'Está URL recibe información de un formulario con el método POST'
-
-
 '''@app.route("/articulos/<int:id>")
 def mostrar_ariculo(id):
 	return 'Vamos a mostrar el artículo con id:{}'.format(id)'''
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    from forms import UploadForm
-    form = UploadForm()
+@app.route('/categorias', methods=['GET'])
+def categorias():
+    from models import Categorias
+    categorias = Categorias.query.all()
+    return render_template("index.html", categorias=categorias)
+
+
+@app.route('/categorias/new', methods=["GET", "POST"])
+def categorias_new():
+    from models import Categorias
+    form = formCategoria(request.form)
     if form.validate_on_submit():
-        f = form.photo.data
-        filename = secure_filename(f.filename)
-        f.save(app.root_path + "/static/img/" + filename)
-        return 'saved' #redirect(url_for('home'))
-    return render_template('send_img.html', form=form)
+        cat = Categorias(nombre=form.nombre.data)
+        db.session.add(cat)
+        db.session.commit()
+        return redirect(url_for("categorias"))
+    else:
+        return render_template("categorias_new.html", form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/categorias/<id>', methods=['GET'])
+def categoria():
+    from models import Categorias, Articulos
+    categoria=Categorias.query.get(id)
+    if id == '0':
+        articulos =Articulos.query.all()
+    else:
+        articulos=Articulos.query.filter_by(CategoriaID=id)
+    categorias=Categorias.query.all()
+    return render_template('categorias.html', articulos=articulos, categoria=categoria, categorias=categorias)
+
+
+@app.route('/articulos/new', methods=["GET", "POST"])
+def articulos_new():
+    from models import Articulos, Categorias
+    form = formArticulo()
+    categorias = [(c.id, c.nombre) for c in Categorias.query.all()[1:]]
+    form.CategoriaId.choices = categorias
+    if form.validate_on_submit():
+        try:
+            f = form.photo.data
+            nombre_fichero = secure_filename(f.filename)
+            f.save(app.root_path + "/static/img/" + nombre_fichero)
+        except:
+            nombre_fichero = ""
+        art = Articulos()
+        form.populate_obj(art)
+        art.image = nombre_fichero
+        db.session.add(art)
+        db.session.commit()
+        return redirect(url_for("home"))
+    else:
+        return render_template("upload.html", form=form)
+
+
+'''@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         return 'Hemos accedido con POST'
     else:
-        return 'Hemos accedido con GET'
+        return 'Hemos accedido con GET'''
 
 
 @app.errorhandler(404)
